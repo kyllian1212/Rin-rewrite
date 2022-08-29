3b8d1eae72ffb75774f6db1e875654248bbcfb9b
kyllian1212 <kyllian1212@live.fr>
2022-08-30 01:41:52 +0200

    increased max cached messages and reconnect=true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 # dont forget second sky countdown
+# archive channel function
 
 import os
 import sys
@@ -18,11 +19,11 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!!", intents=intents)
+bot = commands.Bot(command_prefix="!!", intents=intents, max_messages=10000)
 
 if __name__ == "__main__":
     try:
-        bot.run(TOKEN)
+        bot.run(TOKEN, reconnect=True)
     except:
         raise
 
