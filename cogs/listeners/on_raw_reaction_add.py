c811ec17ec445c74d69cc785003bc5d4491e3442
kyllian1212 <kyllian1212@live.fr>
2022-10-16 22:12:20 +0200

    report message fix

--- cogs/listeners/on_raw_reaction_add.py
+++ cogs/listeners/on_raw_reaction_add.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import discord
 from discord.ext import commands
 from discord.utils import get
@@ -34,21 +35,34 @@ class OnRawReactionAddCog(commands.Cog):
                 message_creation_timestamp = int(reacted_message.created_at.timestamp())
                 reacted_message_author_roles = None
                 long_message = False
+                is_member = True
+                guild = self.bot.get_guild(payload.guild_id)
+                
+                #check if user is a member of the server or not
+                if guild.get_member(reacted_message.author.id) is None:
+                    is_member = False
+                    description += "\n*This user is currently not on the server.*"
 
-                for role in reacted_message.author.roles:
-                    if role.name != '@everyone':
-                        if reacted_message_author_roles is None:
-                            reacted_message_author_roles = str(role.mention)
-                        else:
-                            reacted_message_author_roles = str(reacted_message_author_roles) + ", " + str(role.mention)
+                if is_member == True:
+                    for role in reacted_message.author.roles:
+                        if role.name != '@everyone':
+                            if reacted_message_author_roles is None:
+                                reacted_message_author_roles = str(role.mention)
+                            else:
+                                reacted_message_author_roles = str(reacted_message_author_roles) + ", " + str(role.mention)
 
                 reported_message_embed = discord.Embed(title="Reported Message - ID: " + str(reacted_message.id), description=description, color=0xff0000)
-                reported_message_embed.set_thumbnail(url=reacted_message.author.avatar.url)
+                if reacted_message.author.avatar == None:
+                    reported_message_embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
+                else:
+                    reported_message_embed.set_thumbnail(url=reacted_message.author.avatar.url)
                 reported_message_embed.add_field(name="Username", value=reacted_message.author.name, inline=True)
-                reported_message_embed.add_field(name="Nickname", value=reacted_message.author.nick, inline=True)
+                if is_member == True:
+                    reported_message_embed.add_field(name="Nickname", value=reacted_message.author.nick, inline=True)
                 reported_message_embed.add_field(name="User ID", value="<@" + str(reacted_message.author.id) + ">", inline=True)
                 reported_message_embed.add_field(name="Channel", value="<#" + str(reacted_message.channel.id) + ">", inline=True)
-                reported_message_embed.add_field(name="User Roles", value=str(reacted_message_author_roles), inline=True)
+                if is_member == True:
+                    reported_message_embed.add_field(name="User Roles", value=str(reacted_message_author_roles), inline=True)
                 reported_message_embed.add_field(name="Message creation date/time", value="<t:" + str(message_creation_timestamp) + ":D> - <t:" + str(message_creation_timestamp) + ":T>", inline=True)
                 #if the message is too long
                 if len(reacted_message.content) >= 1000:
