909714ec92d13bdd4ec17fd22da8665123d03514
kyllian1212 <kyllian1212@live.fr>
2024-07-05 22:15:19 +0200

    v2.0.7

--- cogs/tasks/countdown.py
+++ cogs/tasks/countdown.py
@@ -17,7 +17,7 @@ class SmileCountdownCog(commands.Cog):
     async def days_to_smile_auto(self):
         now = datetime.now()
         d1 = datetime.now()
-        d2 = datetime(2024, 7, 26, 6, 0, 0)
+        d2 = datetime(2024, 7, 25, 12, 0, 0)
         diff = d2-d1
         diffd = int((diff.total_seconds())/60/60/24)+1
         diffh = int((diff.total_seconds()+1)/60/60)
@@ -26,15 +26,15 @@ class SmileCountdownCog(commands.Cog):
         diffs_float = float((diff.total_seconds()+1))
         if diffs_float <= 1:
             smile_message_embed = discord.Embed(title="SMILE! :D is out!!", color=0x00aeff)
-            smile_message_embed.add_field(name="placeholder to change before release", value=":3c", inline=False)
+            smile_message_embed.add_field(name="SMILE! :D is out now in more countries!", value="(you can VPN to listen or wait until it's out in your country at midnight local time. do not pirate or send files here or you will get banned)", inline=False)
         elif diffm <= 1:
-            smile_message_embed = discord.Embed(title="There are " + str(diffs) + " seconds left before SMILE! :D is released", color=0x00aeff)
+            smile_message_embed = discord.Embed(title="There are " + str(diffs) + " seconds left before SMILE! :D is released", description="(in New Zealand)", color=0x00aeff)
         elif diffh <= 2:
-            smile_message_embed = discord.Embed(title="There are " + str(diffm) + " minutes (" + str(diffs) + " seconds) left before SMILE! :D is released", color=0x00aeff)
+            smile_message_embed = discord.Embed(title="There are " + str(diffm) + " minutes (" + str(diffs) + " seconds) left before SMILE! :D is released", description="(in New Zealand)", color=0x00aeff)
         elif diffh < 100:
-            smile_message_embed = discord.Embed(title="There are " + str(diffh) + " hours (" + str(diffm) + " minutes, " + str(diffs) + " seconds) left before SMILE! :D is released", color=0x00aeff)
+            smile_message_embed = discord.Embed(title="There are " + str(diffh) + " hours (" + str(diffm) + " minutes, " + str(diffs) + " seconds) left before SMILE! :D is released", description="(in New Zealand)", color=0x00aeff)
         elif diffh >= 100:
-            smile_message_embed = discord.Embed(title="There are " + str(diffd) + " days (" + str(diffh) + " hours, " + str(diffm) + " minutes, " + str(diffs) + " seconds) left before SMILE! :D is released", color=0x00aeff)
+            smile_message_embed = discord.Embed(title="There are " + str(diffd) + " days (" + str(diffh) + " hours, " + str(diffm) + " minutes, " + str(diffs) + " seconds) left before SMILE! :D is released", description="(in New Zealand)", color=0x00aeff)
         smile_message_embed.set_thumbnail(url="https://pbs.twimg.com/media/GL3gAGUa4AAScOx?format=jpg&name=large")
         smile_message_embed.set_footer(text=str(now.strftime("%d/%m/%Y - %H:%M:%S")))
         await self.bot.get_guild(186610204023062528).get_channel(671792848135389184).send(embed=smile_message_embed) #will send in prd's #smile channel
@@ -44,7 +44,7 @@ class SmileCountdownCog(commands.Cog):
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
         dc1 = datetime.now()
-        dc2 = datetime(2024, 7, 26, 6, 0, 0)
+        dc2 = datetime(2024, 7, 25, 12, 0, 0)
         diffc = dc2-dc1
         diffch = int((diffc.total_seconds()+1)/60/60)
         SMILE_TIME_DICT=[
