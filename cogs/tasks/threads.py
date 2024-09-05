9b5e1eb901a49bce6cfed7d17ab559f1ae75df99
Toxin_X <109931081+toxin-x@users.noreply.github.com>
2024-09-05 01:08:27 -0400

    changed names of google files

--- cogs/tasks/threads.py
+++ cogs/tasks/threads.py
@@ -33,7 +33,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
+# If modifying these scopes, delete the file google_auth_tokens.json
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 # The ID and range of a sample spreadsheet.
@@ -105,22 +105,22 @@ class ThreadsCog(commands.Cog):
 
     async def getsheet(self, server_id):
         creds = None
-        # The file token.json stores the user's access and refresh tokens, and is
+        # The file google_auth_tokens.json stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists("google_token.json"):
-            creds = Credentials.from_authorized_user_file("google_token.json", SCOPES)
+        if os.path.exists("google_auth_tokens.json"):
+            creds = Credentials.from_authorized_user_file("google_auth_tokens.json", SCOPES)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    "google_credentials.json", SCOPES
+                    "google_client_secret.json", SCOPES
                 )
                 creds = flow.run_console()
             # Save the credentials for the next run
-        with open("google_token.json", "w") as token:
+        with open("google_auth_tokens.json", "w") as token:
             token.write(creds.to_json())
         try:
             service = build("sheets", "v4", credentials=creds)
