a278f61277f4832f5731d815885a6b1a1ff59b2d
Toxin_X <109931081+toxin-x@users.noreply.github.com>
2025-07-12 00:22:29 -0400

    add KMFA and the album for belive it to song library

--- db/default_song_library.sql
+++ db/default_song_library.sql
@@ -67,10 +67,12 @@ INSERT INTO bot_default_song_library (artist, song_title, album, length_in_secon
 ("Porter Robinson", "Mona Lisa", "SMILE! :D", 225),
 ("Porter Robinson", "Is There Really No Happiness?", "SMILE! :D", 199),
 ("Porter Robinson", "Everything To Me", "SMILE! :D", 292),
-  
+
 /* skrillex, porter robinson & bibi bourelly*/
 -- still here
 ("Skrillex, Porter Robinson & Bibi Bourelly", "Still Here (with the ones that I came with)", "Quest For Fire", 312),
+/* Michael Clifford & Porter Robinson */
+("Michael Clifford & Porter Robinson", "kill me for always", "SIDEQUEST", 187),
 /* porter robinson & madeon */
 -- shelter
 ("Porter Robinson & Madeon", "Shelter", "Shelter", 219),
@@ -118,7 +120,14 @@ INSERT INTO bot_default_song_library (artist, song_title, album, length_in_secon
 ("Madeon", "Love You Back", "Love You Back", 214),
 ("Madeon", "Gonna Be Good", "Gonna Be Good", 172),
 ("Madeon", "Gonna Be Good (Live Edit)", "Gonna Be Good (Live Edit)", 271),
+-- GAP YEAR
 -- all ur luv
 ("Wavedash, Madeon & Toro Y Moi", "All Ur Luv", "Tempo", 161),
--- believe it (album unknown)
-("Louis The Child, Madeon", "Believe It", "Believe It", 150);
+-- believe it
+("Louis The Child, Madeon", "Believe It", "The Sun Comes Up", 150);
+/*
+-- Credited but not featured
+("keshi", "Kiss Me Right", "Requiem (Bonus Edition)", 162),
+("Petit Biscuit", "Cruel Heart", "Discipline", 213),
+("chromeo", "Friendsnlovers", "Adult Contemporary", 155);
+*/
